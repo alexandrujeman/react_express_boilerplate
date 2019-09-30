63afa8d6576c0dd36285258c15ca50936c4149cc
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-09-30 22:35:02 +0300

    add react

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express')
 
 const app = express()
 
+app.get('/api/customers', (req, res) => {
+  const customers = [
+    { id: 1, firstName: 'John', lastName: 'Doe' },
+    { id: 2, firstName: 'Steve', lastName: 'Smith' },
+    { id: 3, firstName: 'Mary', lastName: 'Swanson' },
+  ]
+  res.json(customers)
+})
+
 // Node connection
 const hostname = '0.0.0.0'
 const port = 5000
